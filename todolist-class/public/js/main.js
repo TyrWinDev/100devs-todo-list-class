828f5625f116c48d6eaa1a1835d74491fb9f4193
TyrWinDev <Tyrwinterhd@gmail.com>
2021-04-07 01:44:33 -0400

    changes

---
+++ todolist-class/public/js/main.js
@@ -0,0 +1,76 @@
+//Selects all the elements with the class of del
+const deleteBtn = document.querySelectorAll('.del')
+//Selects all the span elements inside an element with the todoItem Class
+const todoItem = document.querySelectorAll('.todoItem span')
+
+const todoComplete = document.querySelectorAll('.todoItem span.completed')
+
+//Group elements in an Array, and iterates through them adding an event listener
+Array.from(deleteBtn).forEach((el) => {
+    el.addEventListener('click', deleteTodo)
+})
+
+Array.from(todoItem).forEach((el)=>{
+    el.addEventListener('click', markComplete)
+})
+
+Array.from(todoComplete).forEach((el)=>{
+    el.addEventListener('click', undo)
+})
+
+async function deleteTodo(){
+    //We grab the text from the sibling element before our delete class. 
+    const todoText = this.parentNode.childNodes[1].innerText
+
+    try {
+        const response = await fetch('deleteTodo', {
+            method: 'delete',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({
+                'rainbowUnicorn': todoText
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+    }catch(err){
+        console.log(err)
+    }
+}
+
+async function markComplete(){
+    const todoText = this.parentNode.childNodes[1].innerText
+    try{
+        const response = await fetch('markComplete', {
+            method: 'put',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({
+                'rainbowUnicorn': todoText
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+    }catch(err){
+        console.log(err)
+    }
+}
+
+
+async function undo(){
+    const todoText = this.parentNode.childNodes[1].innerText
+    try{
+        const response = await fetch('undo', {
+            method: 'put',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({
+                'rainbowUnicorn': todoText
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+    }catch(err){
+        console.log(err)
+    }
+}
